29c62578097d9e313f706738b4c2070c028bd5c0
juancamilolindo <jkm1l0@gmail.com>
2025-10-24 18:31:05 -0500

    Prueba del despliegue del frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React in MemoryBook</h1>
+      <h1>Vite + React in MemoryBook Deploy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
